d0b8f74016f46064e883d9f2c9953d27bcf74ad5
andreasbott <andreasbottkristensen@gmail.com>
2021-12-30 23:26:50 +0100

    and everything is working now

--- react-portfolio-app/App.tsx
+++ react-portfolio-app/App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World! And it is working!</Text>
+      <Text>Hello World! And it is working and it is working again!</Text>
     </View>
   );
 }
