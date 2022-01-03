7a2420baeab7426e7a1f1f9286a4acac0de11a8e
andreasbott <andreasbottkristensen@gmail.com>
2022-01-03 21:42:23 +0100

    no message

---
+++ react-portfolio-app/web-build/static/js/app.7c82bc79.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonp=this.webpackJsonp||[]).push([[0],{39:function(n,e,t){"use strict";t.d(e,"a",(function(){return i}));var r=t(5),c=t(56),o=t(19),a=t(37);function i(){return Object(a.jsx)(o.a,{style:s.container,children:Object(a.jsx)(c.a,{children:"Hello World and AWS"})})}var s=r.a.create({container:{flex:1,backgroundColor:"#fff",alignItems:"center",justifyContent:"center"}})},62:function(n,e,t){n.exports=t(85)}},[[62,1,2]]]);
+//# sourceMappingURL=app.7c82bc79.chunk.js.map
